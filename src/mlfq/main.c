93a2dfaa26fbf6a81046a559a49379ffb58593a2
Marcelo Saldías <misaldias@uc.cl>
2022-04-06 00:10:25 -0400

    Obtener argumentos de enunciado

--- src/mlfq/main.c
+++ src/mlfq/main.c
@@ -4,11 +4,22 @@
 #include "../queue/queue.h"
 #include "../file_manager/manager.h"
 
+// Parámetro del programa
+int q;
+char* output_file;
+
+
 int main(int argc, char const *argv[])
 {
 	/*Lectura del input*/
 	char *file_name = (char *)argv[1];
 	InputFile *input_file = read_file(file_name);
+
+	output_file = (char *)argv[2];
+	q = atoi((char *) argv[3]); 
+
+	printf("Archivo output: %s\n", output_file);
+	printf("Valor de q: %d\n", q);
 
 	/*Mostramos el archivo de input en consola*/
 	printf("Nombre archivo: %s\n", file_name);
