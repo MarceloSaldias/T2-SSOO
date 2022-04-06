a4b9aafa755d6bed380cc8d96301fccc1e04d5c4
Marcelo Saldías <misaldias@uc.cl>
2022-04-05 20:26:24 -0400

    Arreglo de errores y funciones publicas

--- src/process/process.c
+++ src/process/process.c
@@ -1,1 +1,34 @@
+// Libreria estandar de c
+#include <stdlib.h>
+// Liberia de input y output
+#include <stdio.h>
+// Libreria de Strings
+#include <string.h>
+
 #include "process.h"
+
+/** Constructor de un proceso */
+Process* process_init(int pid, char name[33], int priority, enum Status status, int aging)
+{
+  Process* process = malloc(sizeof(Process));
+  
+  // Inicializamos el proceso
+  process -> pid = pid;
+  strcpy(process -> name, name);
+  process -> priority = priority;
+  // TODO: Ver si esto se define altiro o en tiempo de ejecución.
+  process -> start_time = 0;
+  process -> cycles = 0;
+  process -> wait = 0;
+  process -> status = status;
+  process -> aging = aging;
+
+  return process;
+}
+
+/** Funcion que destruye el proceso la memoria utilizada */
+void process_destroy(Process* process)
+{
+  // Liberamos la memoria utilizada para almacenar el struct
+  free(process);
+}
--- src/process/process.h
+++ src/process/process.h
@@ -5,14 +5,14 @@ enum Status {
   READY,
   WAITING,
   FINISHED
-}
+};
 
 /** Estructura de un proceso **/
 typedef struct process {
     // Process pid
     int pid;
     // Process name
-    char[32] name;
+    char name[33];
     // Process priority
     int priority;
     
@@ -20,8 +20,14 @@ typedef struct process {
     int cycles;
     int wait;
     int waiting_delay;
-    Status status;
+    enum Status status;
     int aging;
 } Process;
 
-// TODO: Faltan todas las fucnciones publicas
+///////////////////////////// Funciones publicas ///////////////////////////
+
+/** Constructor de un proceso */
+Process* process_init(int pid, char name[33], int priority, enum Status status, int aging);
+
+/** Funcion que destruye el proceso la memoria utilizada */
+void process_destroy(Process* process);
